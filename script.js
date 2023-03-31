594a52e92d195eb49e0f32d128f650a8cd6697fa
Rakesh P R <rakeshpr369@gmail.com>
2023-03-31 20:50:52 +0530

    bank

---
+++ script.js
@@ -0,0 +1,29 @@
+let balance = 0;
+
+        function deposit() {
+            var amount = parseFloat(document.getElementById("depositInput").value);
+            if (isNaN(amount) || amount <= 0) {
+                alert("Please enter a valid amount.");
+                return;
+            }
+            balance += amount;
+            document.getElementById("balance").innerHTML = balance.toFixed(2);
+        }
+
+        function withdraw() {
+            var amount = parseFloat(document.getElementById("withdrawInput").value);
+            if (isNaN(amount) || amount <= 0) {
+                alert("Please enter a valid amount.");
+                return;
+            }
+            if (amount > balance) {
+                alert("Insufficient funds.");
+                return;
+            }
+            balance -= amount;
+            document.getElementById("balance").innerHTML = balance.toFixed(2);
+        }
+
+        function showBalance() {
+            alert("Current balance: Rs." + balance.toFixed(2));
+        }
